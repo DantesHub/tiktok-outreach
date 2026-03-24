28d5428f30a1db90406c81fe7ec854cdf8d97611
Saad Aziz <46464104+azizsaad@users.noreply.github.com>
2026-03-24 18:20:07 +0100

    progress

--- src/template.ts
+++ src/template.ts
@@ -1,35 +1,35 @@
 export function renderEmail(firstName: string): { subject: string; body: string; html: string } {
   const appUrl = "https://apps.apple.com/gb/app/lumo-daily-light/id6757131632";
+  const tiktokUrl = "https://www.tiktok.com/@saad.sochill";
 
   return {
     subject: `${firstName} x Lumo | Paid Promo?`,
     body: `Hey ${firstName},
 
-I love your content and would love to do a promotion with you !!
+I love your content and would love to do UGC-content with you.
 
-Our app is "Finch for Muslims" - keep up with daily Islamic goals with friends!
+It's for my app Lumo - it's like "Finch for Muslims" - keep up with daily spiritual goals with friends.  and you don't need to be Muslim to make UGC-content !!!
 
-It's called "Lumo Daily Light" if you want to check it out on the app store:
-
+Check it out on the app store!
 ${appUrl}
+Basically, I'll provide an example TikTok which you literally make a copy of!
+
+One of my creators has already made $400 in 2 weeks!
 
 If interested, let's hop in a 2 minute call!
 
-Thanks,
-Saad
+Thanks, Saad
 
-p.s. I post about it on my TikTok - check it out!
-
-https://www.tiktok.com/@saad.sochill`,
+p.s. I post about the app on my TikTok - check it out!
+${tiktokUrl}`,
     html: `<p>Hey ${firstName},</p>
-<p>I love your content and would love to do a promotion with you !!</p>
-<p>Our app is "Finch for Muslims" - keep up with daily Islamic goals with friends!</p>
-<p>It's called <b>"Lumo Daily Light"</b> if you want to check it out on the app store:</p>
-<p><a href="${appUrl}">${appUrl}</a></p>
+<p>I love your content and would love to do UGC-content with you.</p>
+<p>It's for my app Lumo - it's like "Finch for Muslims" - keep up with daily spiritual goals with friends.  and you don't need to be Muslim to make UGC-content !!!</p>
+<p>Check it out on the app store!<br><a href="${appUrl}">${appUrl}</a></p>
+<p>Basically, I'll provide an example TikTok which you literally make a copy of!</p>
+<p>One of my creators has already made $400 in 2 weeks!</p>
 <p>If interested, let's hop in a 2 minute call!</p>
-<p>Thanks,<br>Saad</p>
-<br>
-<p>p.s. I post about it on my TikTok - check it out!</p>
-<p><a href="https://www.tiktok.com/@saad.sochill">https://www.tiktok.com/@saad.sochill</a></p>`,
+<p>Thanks, Saad</p>
+<p>p.s. I post about the app on my TikTok - check it out!<br><a href="${tiktokUrl}">${tiktokUrl}</a></p>`,
   };
 }
